3cf371887868ff307f4a420c44fc3133b461d704
Gabriel Nassar <g.nassar@uniandes.edu.co>
2023-11-24 08:49:45 -0500

    Update album.service.ts

--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -13,7 +13,38 @@ export class AlbumService {
     ) { }
 
     async findAll(): Promise<AlbumEntity[]> {
-        return await this.albumRepository.find({relations: ['performer']});
+        return await this.albumRepository.find({relations: ['performer', 'tracks']});
+    }
+
+    async findOne(id: string): Promise<AlbumEntity> {
+        const album: AlbumEntity = await this.albumRepository.findOne({where: {id}, relations: ['performer', 'tracks']});
+        if (!album) {
+            throw new BusinessLogicException('The album with the given id was not found', BusinessError.NOT_FOUND);
+        }
+        return album;
+    }
+
+    async create(album: AlbumEntity): Promise<AlbumEntity> {
+        const newAlbum: AlbumEntity = await this.albumRepository.create(album);        
+        if (newAlbum.nombre === '') {
+            throw new BusinessLogicException('The album name cannot be empty', BusinessError.BAD_REQUEST);
+        }       
+        if (newAlbum.descripcion === '') {
+            throw new BusinessLogicException('The album description cannot be empty', BusinessError.BAD_REQUEST);
+        }
+
+        return await this.albumRepository.save(newAlbum);
+    }
+
+    async delete(id: string) {
+        const album: AlbumEntity = await this.albumRepository.findOne({where:{id}});
+        if (!album) {
+            throw new BusinessLogicException('The album with the given id was not found', BusinessError.NOT_FOUND);
+        }        
+        if (album.tracks.length > 0) {
+            throw new BusinessLogicException('The album cannot be deleted because it has associated tracks', BusinessError.BAD_REQUEST);
+        }
+        await this.albumRepository.remove(album);
     }
 
 }
