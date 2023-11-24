b9e85dbef5c6736822a371a08a1f66c2e2f6f9cd
Gabriel Nassar <g.nassar@uniandes.edu.co>
2023-11-24 08:58:45 -0500

    perf track

--- src/performer/performer.service.ts
+++ src/performer/performer.service.ts
@@ -1,4 +1,35 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BusinessError, BusinessLogicException } from '../shared/errors/business-errors';
+import { Repository } from 'typeorm';
+import { PerformerEntity } from './performer.entity';
 
 @Injectable()
-export class PerformerService {}
+export class PerformerService {
+    constructor(
+        @InjectRepository(PerformerEntity)
+        private readonly performerRepository: Repository<PerformerEntity>
+    ) { }
+
+    async findAll(): Promise<PerformerEntity[]> {
+        return await this.performerRepository.find({relations: ['albums']});
+    }
+
+    async findOne(id: string): Promise<PerformerEntity> {
+        const performer: PerformerEntity = await this.performerRepository.findOne({where: {id}, relations: ['albums']});
+        if (!performer) {
+            throw new BusinessLogicException('The performer with the given id was not found', BusinessError.NOT_FOUND);
+        }
+        return performer;
+    }
+
+    async create(performer: PerformerEntity): Promise<PerformerEntity> {
+        const newPerformer: PerformerEntity = await this.performerRepository.create(performer);                
+        if (newPerformer.descripcion.length > 100) {
+            throw new BusinessLogicException('The performer description cannot be more than 100 characters', BusinessError.BAD_REQUEST);
+        }
+        return await this.performerRepository.save(newPerformer);
+    }
+
+}
--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,4 +1,39 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BusinessError, BusinessLogicException } from '../shared/errors/business-errors';
+import { Repository } from 'typeorm';
+import { TrackEntity } from './track.entity';
 
 @Injectable()
-export class TrackService {}
+export class TrackService {
+    constructor(
+        @InjectRepository(TrackEntity)
+        private readonly trackRepository: Repository<TrackEntity>
+    ) { }
+
+    async findAll(): Promise<TrackEntity[]> {
+        return await this.trackRepository.find({relations: ['album']});
+    }
+
+    async findOne(id: string): Promise<TrackEntity> {
+        const track: TrackEntity = await this.trackRepository.findOne({where: {id}, relations: ['album']});
+        if (!track) {
+            throw new BusinessLogicException('The track with the given id was not found', BusinessError.NOT_FOUND);
+        }
+        return track;
+    }
+
+    async create(track: TrackEntity, albumId: string): Promise<TrackEntity> {
+        const newTrack: TrackEntity = await this.trackRepository.create(track);        
+        if (newTrack.duracion <= 0) {
+            throw new BusinessLogicException('The track duration must be positive', BusinessError.BAD_REQUEST);
+        }
+        const album: TrackEntity = await this.trackRepository.findOne({where: {id: albumId}});
+        if (!album) {
+            throw new BusinessLogicException('The album associated with the track was not found', BusinessError.NOT_FOUND);
+        }
+        
+        return await this.trackRepository.save(newTrack);
+    }
+}
